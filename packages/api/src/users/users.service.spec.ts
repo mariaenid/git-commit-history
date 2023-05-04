2510b5cc7d3e09d95dc12ceee13938ce7d7eaadd
Maria Pineda <mepineda1992@gmail.com>
2023-05-03 21:06:02 -0500

    Implement command to test the api

--- packages/api/src/users/users.service.spec.ts
+++ packages/api/src/users/users.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { OrmModule } from '../orm/orm.module';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [OrmModule],
+      providers: [UsersService, Repository],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
