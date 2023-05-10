7e668679752a64e8058c3a45ae93fe6cf7aa82fd
Maria Pineda <mepineda1992@gmail.com>
2023-05-10 13:10:04 -0500

    Fix react dependencies

--- packages/cloud/src/main.tsx
+++ packages/cloud/src/main.tsx
@@ -1,20 +1,18 @@
 import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.getElementById('root');
+render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  </StrictMode>, root
 );
